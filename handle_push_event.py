17dec842d59c1b125f8fc6dc80d9b34822a6a307
smartwu_sir <smartwu_sir@163.com>
2019-05-31 15:06:27 +0800

    添加初始接口（flask）

---
+++ handle_push_event.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/github/event/push", methods=["POST"])
+def handle_push_event():
+    print("request.get_json: ", request.get_json())
+    return "SUCCESS"
+
+
+if __name__ == "__main__":
+    app.run('localhost', 7701, debug=True)
