e551b9a36166f35615c37e23263b0cbec0626f37
smartwu_sir <smartwu_sir@163.com>
2019-05-31 16:20:31 +0800

    打印出分支名和仓库名

--- handle_push_event.py
+++ handle_push_event.py
@@ -5,8 +5,17 @@ app = Flask(__name__)
 
 @app.route("/github/event/push", methods=["POST"])
 def handle_push_event():
-    print("request.data: ", request.data)
-    print("\nrequest.get_json: ", request.get_json())
+    try:
+        content = request.get_json()
+        branch = content["ref"].split("/")[-1]
+        repository_name = content["repository"]["name"]
+        print("branch: ", branch)
+        print("repository_name: ", repository_name)
+    except Exception as e:
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>> ERROR")
+        print(e)
+        print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+
     return "SUCCESS"
 
 
