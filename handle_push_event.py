669979e93c5d896306ebd49ac9fdec2f419ba0f2
smartwu_sir <smartwu_sir@163.com>
2019-05-31 17:49:40 +0800

    fix

--- handle_push_event.py
+++ handle_push_event.py
@@ -8,15 +8,13 @@ app = Flask(__name__)
 @app.route("/github/event/push", methods=["POST"])
 def handle_push_event():
     # 解析 github 发过来的数据，提取出分支名 branch 和项目名 repository_name, 交给 exec_shell 去处理
-    try:
 
+    try:
         content = request.get_json()
         branch = content["ref"].split("/")[-1]
         repository_name = content["repository"]["name"]
         exec_shell(branch, repository_name)
-
     except Exception as e:
-
         print(">>>>>>>>>>>>>>>>>>>>>>>>>> ERROR")
         print(e)
         print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
