c4eba89996eae0690b091b6586b73f7e9d47dea2
smartwu_sir <smartwu_sir@163.com>
2019-05-31 16:13:49 +0800

    add alpha

--- handle_push_event.py
+++ handle_push_event.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/github/event/push", methods=["POST"])
 def handle_push_event():
-    print("request.data: ", request.data)
+
+    print("alpha:request.data: ", request.data)
     print("\nrequest.get_json: ", request.get_json())
     return "SUCCESS"
 
