1ea822a8d7cc579e47c34a50206084564f0a7701
smartwu_sir <smartwu_sir@163.com>
2019-07-09 16:06:45 +0800

    增加 Stock 项目的自动重启

--- exec_shell.py
+++ exec_shell.py
@@ -5,13 +5,16 @@ import os
 # 定义每个不同的仓库名字执行的 shell 语句
 SHELL_DICT = {
     "my_blog": "cd /home/wcp/work/github-projects/my_blog && "
-               "/home/wcp/.virtualenvs/py3_django2/bin/uwsgi --stop /home/wcp/var/pids/uwsgi_pidfile.log && "
                "git pull origin master && "
+               "/home/wcp/.virtualenvs/py3_django2/bin/uwsgi --stop /home/wcp/var/pids/uwsgi_pidfile.log && "
                "supervisorctl start wcp_blog_uwsgi",
     "SharePic": "cd /home/wcp/work/github-projects/SharePic && "
-               "/home/wcp/.virtualenvs/py3_django2/bin/uwsgi --stop /home/wcp/var/pids/uwsgi_SharePic.pid && "
                 "git pull origin master && "
+               "/home/wcp/.virtualenvs/py3_django2/bin/uwsgi --stop /home/wcp/var/pids/uwsgi_SharePic.pid && "
                 "supervisorctl restart wcp_SharePic_uwsgi wcp_SharePic_celery_worker",
+    "Stock": "cd /home/wcp/work/git-projects/Stock && "
+             "git pull origin master &&"
+             "supervisorctl restart Stock_project:*"
 }
 
 
